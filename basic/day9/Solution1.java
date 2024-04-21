1be99bc6c477a6a8d4214984ade22237e9c47b65
Lee-1sj <sukjin0112@naver.com>
2024-04-21 15:04:45 +0900

    [update] basic day9 solution1

---
+++ basic/day9/Solution1.java
@@ -0,0 +1,18 @@
+package basic.day9;
+
+import java.util.ArrayList;
+
+public class Solution1 {
+    public ArrayList<Integer> solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for( String data : intStrs){
+            String answer = data.substring(s, s+l);
+            int num = Integer.parseInt(answer);
+            
+            if(num > k){
+                list.add(num);
+            }        
+        }
+        return list;
+    }    
+}
