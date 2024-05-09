e655e372d048c6bce15e88160495c9fba59cea46
Lee-1sj <sukjin0112@naver.com>
2024-05-09 20:03:04 +0900

    [update] 프로그래머스 기초 day14

---
+++ basic/day14/Solution2.java
@@ -0,0 +1,17 @@
+package basic.day14;
+
+import java.util.ArrayList;
+
+public class Solution2 {
+    public String[] solution(String[] names) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < names.length; i += 5) {
+            list.add(names[i]);
+        }
+        String[] answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
